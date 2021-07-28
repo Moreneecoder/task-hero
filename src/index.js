cd0f1b74ba0acdfb63a4e49fd1bebf74aefe89a8
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-28 23:33:18 +0100

    Setup bootstrap and popper

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function component() {
     const element = document.createElement('div');
  
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+    element.innerHTML = 'Hello World';
+   element.classList.add('hello', 'alert', 'alert-success');
  
     return element;
   }

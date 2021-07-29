dcef1337a6fb61340354de82023f3f04793a0b32
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-29 09:58:39 +0100

    Dynamically load navbar on html page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/collapse';
+import navComponent from './components/nav';
 
-function component() {
-    const element = document.createElement('div');
- 
-    // Lodash, now imported by this script
-    element.innerHTML = 'Hello World';
-   element.classList.add('hello', 'alert', 'alert-success');
- 
-    return element;
-  }
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.prepend(navComponent());
+  
+    // const mainContent = document.getElementById('content');
+    // mainContent.classList.add('container-fluid', 'pb-5', 'pt-5');
+    // mainContent.appendChild(homeComponent());
+  
+    // document.body.appendChild(footerComponent());
+  
+    // document.addEventListener('click', (e) => {
+    //   if (e.target && e.target.id === 'menu') {
+    //     domActions.removeChildWithinParent(mainContent, ['home-wrapper', 'contact-wrapper']);
+    //     domActions.addTabContent(mainContent, 'menu-wrapper', menuComponent());
+    //   }
+  
+    //   if (e.target && e.target.id === 'home') {
+    //     domActions.removeChildWithinParent(mainContent, ['menu-wrapper', 'contact-wrapper']);
+    //     domActions.addTabContent(mainContent, 'home-wrapper', homeComponent());
+    //   }
+  
+    //   if (e.target && e.target.id === 'contact') {
+    //     domActions.removeChildWithinParent(mainContent, ['menu-wrapper', 'home-wrapper']);
+    //     domActions.addTabContent(mainContent, 'contact-wrapper', contactComponent());
+    //   }
+    // });
+  });
  
 //   document.body.appendChild(component());

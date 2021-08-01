07e0c68acbfaaf7821590d20823b2b586bf44aca
Morenikeji Bello <caeserselassie@gmail.com>
2021-08-02 00:02:12 +0100

    Create a task module to handle logic for user tasks

---
+++ src/components/appCompontents/factories/task.js
@@ -0,0 +1,13 @@
+const createNewTask = (taskObj) => {
+    // check if project exists in localstorage
+    // if yes, store new task in corresponding project
+    // if no, create project and store new task in it
+
+    // load and display project task
+}
+
+const loadTasks = (task) => {
+    
+}
+
+export { createNewTask }
